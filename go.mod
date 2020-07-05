6c9379b614f034d98d8131aa911f7420546d69be
pharmeasy-sanjib <sanjib.devnath@pharmeasy.in>
2020-07-05 23:32:48 +0530

    first commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module bitbucket.org/sanjib90/try_go_lang/m/v0
+module sample/v0
 
 go 1.14
+
+require github.com/arangodb/go-driver v0.0.0-20200624173407-d1c92a8bd2b8 // indirect
--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	driver "github.com/arangodb/go-driver"
+	arangohttp "github.com/arangodb/go-driver/http"
+	"net/http"
 	"sync"
 )
 
@@ -22,12 +27,18 @@ func main() {
 	//fmt.Println(strings.Contains(sampleString, "llo"))
 	//fmt.Println(strings.Index(sampleString, "llo"))
 
+	// Open "books" collection
+	//col, err := db.Collection(ctx, "tasks")
+	//if err != nil {
+	//	// Handle error
+	//}
+
 	/**
 	HTTP Server
 	*/
-	//http.HandleFunc("/", handler)
-	//http.HandleFunc("/hi", handler2)
-	//_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/hi", handler2)
+	_ = http.ListenAndServe(":8080", nil)
 	//stringChan := make(chan string)
 	//for i:= 0; i < 3; i++ {
 	//	go makeDough(stringChan)
@@ -44,19 +55,20 @@ func main() {
 	//wg.Wait()
 
 	// channels
-	defer cleanUp()
-	fooval := make(chan int, 10)
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go foo(fooval, i)
-	}
-	wg.Wait()
-	close(fooval)
-	for item := range fooval {
-		fmt.Println(item)
-	}
+	//defer cleanUp()
+	//fooval := make(chan int, 10)
+	//
+	//for i := 0; i < 10; i++ {
+	//	wg.Add(1)
+	//	go foo(fooval, i)
+	//}
+	//wg.Wait()
+	//close(fooval)
+	//for item := range fooval {
+	//	fmt.Println(item)
+	//}
 }
+
 func foo(c chan  int, val int)  {
 	defer wg.Done()
 	defer cleanUp()
@@ -113,15 +125,61 @@ func cleanUp() {
 //		time.Sleep(time.Millisecond * 1000)
 //	}
 //}
-//
-//func handler(w http.ResponseWriter, r *http.Request) {
-//	_, _ = fmt.Fprintf(w, "Hellp \n")
-//}
-//
-//func handler2(w http.ResponseWriter, r *http.Request) {
-//	_, _ = fmt.Fprintf(w, "HellThere whats up \n")
-//}
-//
+
+func connect() (driver.Database, error) {
+	conn, err := arangohttp.NewConnection(arangohttp.ConnectionConfig{
+		Endpoints: []string{"http://localhost:8529"},
+	})
+	if err != nil {
+		// Handle error
+	}
+	c, err := driver.NewClient(driver.ClientConfig{
+		Connection: conn,
+		Authentication: driver.BasicAuthentication("root", ""),
+	})
+	if err != nil {
+		// Handle error
+	}
+
+	// Open "examples_books" database
+	ctx := context.Background()
+	return c.Database(ctx, "spectre")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	db, err := connect()
+	if err != nil {
+		// Handle error
+	}
+
+	ctx := context.Background()
+	query := "for x in tasks return x"
+	var cursor driver.Cursor
+	cursor, err = db.Query(ctx, query, nil)
+	if err != nil {
+		// handle error
+	}
+	defer cursor.Close()
+	var res []interface{}
+	for {
+		var doc interface{}
+		var _, err = cursor.ReadDocument(ctx, &doc)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			// handle other errors
+		}
+		res = append(res, doc)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
+
+func handler2(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "HellThere whats up \n")
+}
+
 //type shape interface {
 //	area() float64
 //}
